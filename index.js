8dd60d1494d9aea2f40576e3069fdba591264e3b
Jose <joseluis0528@gmail.com>
2023-03-24 22:13:37 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(match => match.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(match => match.charAt() === string.charAt());
+}
+
+function matchName(array, string) {
+    return array.filter(match => match.name === string);
+}
